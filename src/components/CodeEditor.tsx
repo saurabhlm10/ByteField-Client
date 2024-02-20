23449b868e40089708e8fd74cafcda5751bc9c38
saurabhlm10 <saurabhlm10@gmail.com>
2024-02-20 22:24:07 +0530

    feat: Integrate code execution in frontend
    - Enhanced the CodeEditor component to submit JavaScript code for execution.
    - Implemented functionality to display execution results and errors from the backend.
    - Updated axios instance with baseURL for API requests.
    - Improved Navbar and layout components for better navigation and page structure.

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -2,11 +2,12 @@
 
 import React, { useState } from "react";
 import Editor from "@monaco-editor/react";
-import axios from "axios";
 import axiosInstance from "@/axios";
+import { AxiosError } from "axios";
 
 const CodeEditor: React.FC = () => {
   const [code, setCode] = useState("// Write your code here");
+  const [executionResult, setExecutionResult] = useState("");
 
   const handleEditorChange = (value: string | undefined) => {
     setCode(value || "");
@@ -17,26 +18,48 @@ const CodeEditor: React.FC = () => {
       const response = await axiosInstance.post("/execute", {
         code,
       });
+
       console.log(response.data);
+      setExecutionResult(response.data || response.data.stderr || "No output");
       // Display execution result to the user
-    } catch (error) {
+    } catch (error: any) {
+      if (error instanceof AxiosError) {
+        return setExecutionResult(
+          error.response?.data.message || "An error occurred"
+        );
+      }
       console.error("Error executing code:", error);
-      // Handle error
+      return setExecutionResult(error.message);
     }
+    // Handle error
   };
 
   return (
     <>
-      <Editor
-        height="70vh"
-        defaultLanguage="javascript"
-        defaultValue={code}
-        onChange={handleEditorChange}
-        theme="vs-dark"
-      />
-      <button onClick={executeCode} className="mt-4 p-2 bg-blue-500 text-white">
+      <button
+        onClick={executeCode}
+        className="self-end mb-2 p-2 bg-blue-500 text-white"
+      >
         Run Code
       </button>
+      <div className="flex">
+        <div className="flex-1 flex flex-col">
+          <Editor
+            height="calc(70vh)" // Adjust height to account for button
+            defaultLanguage="javascript"
+            defaultValue="// Write your code here"
+            onChange={handleEditorChange}
+            theme="vs-dark"
+          />
+        </div>
+        <div
+          className="flex-1 bg-gray-800 text-white p-4 overflow-auto"
+          style={{ height: "70vh" }}
+        >
+          <h3 className="text-lg font-bold">Terminal</h3>
+          <pre>{executionResult}</pre>
+        </div>
+      </div>
     </>
   );
 };
