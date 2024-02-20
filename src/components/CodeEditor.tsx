1b9ceca3a2c29a10265fa0c4f3d8e68ec8e0d509
saurabhlm10 <saurabhlm10@gmail.com>
2024-02-20 23:26:35 +0530

    added responsiveness

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -7,7 +7,8 @@ import { AxiosError } from "axios";
 
 const CodeEditor: React.FC = () => {
   const [code, setCode] = useState("// Write your code here");
-  const [executionResult, setExecutionResult] = useState("");
+  const [stdout, setStdout] = useState(""); // For standard output
+  const [stderr, setStderr] = useState(""); // For error messages
 
   const handleEditorChange = (value: string | undefined) => {
     setCode(value || "");
@@ -20,16 +21,16 @@ const CodeEditor: React.FC = () => {
       });
 
       console.log(response.data);
-      setExecutionResult(response.data || response.data.stderr || "No output");
+      setStdout(response.data || "No output");
+      stderr && setStderr("");
       // Display execution result to the user
     } catch (error: any) {
+      setStdout("");
       if (error instanceof AxiosError) {
-        return setExecutionResult(
-          error.response?.data.message || "An error occurred"
-        );
+        return setStderr(error.response?.data.message || "An error occurred");
       }
       console.error("Error executing code:", error);
-      return setExecutionResult(error.message);
+      return setStdout(error.message);
     }
     // Handle error
   };
@@ -38,26 +39,27 @@ const CodeEditor: React.FC = () => {
     <>
       <button
         onClick={executeCode}
-        className="self-end mb-2 p-2 bg-blue-500 text-white"
+        className="order-2 md:order-1 w-full md:w-auto self-end mb-2 p-2 bg-blue-500 text-white"
       >
         Run Code
       </button>
-      <div className="flex">
-        <div className="flex-1 flex flex-col">
+      <div className="flex flex-col md:flex-row">
+        <div className="order-1 md:order-2 flex-1">
           <Editor
-            height="calc(70vh)" // Adjust height to account for button
+            height="70vh"
             defaultLanguage="javascript"
-            defaultValue="// Write your code here"
+            defaultValue={code}
             onChange={handleEditorChange}
             theme="vs-dark"
           />
         </div>
         <div
-          className="flex-1 bg-gray-800 text-white p-4 overflow-auto"
+          className="order-3 md:w-1/3 bg-gray-800 text-white p-4 overflow-auto"
           style={{ height: "70vh" }}
         >
           <h3 className="text-lg font-bold">Terminal</h3>
-          <pre>{executionResult}</pre>
+          <pre>{stdout}</pre>
+          <pre className="text-red-500">{stderr}</pre>
         </div>
       </div>
     </>
