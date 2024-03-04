20d7eb5a2dc8ab0b55f32b029cafb4bfe85c52dc
saurabhlm10 <saurabhlm10@gmail.com>
2024-03-05 03:05:20 +0530

    add .env

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 export default axiosInstance;
